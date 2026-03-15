9b5d9d529c3d500a8b7d3f1394b0c02db3f7a6a9
Farheen Mahjarin Trisha <farheenmahjarin4@gmail.com>
2026-03-15 16:19:06 +0600

    footer updated

--- src/components/CopyEmailButton.jsx
+++ src/components/CopyEmailButton.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { AnimatePresence, motion } from "motion/react";
 const CopyEmailButton = () => {
   const [copied, setCopied] = useState(false);
-  const email = "Your Email Address";
+  const email = "farheentrisha2.0@gmail.com";
 
   const copyToClipboard = () => {
     navigator.clipboard.writeText(email);
