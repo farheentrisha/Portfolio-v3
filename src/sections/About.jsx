d5182729c90f89183f44814f59276c127c95b8fd
Farheen Mahjarin Trisha <farheenmahjarin4@gmail.com>
2026-03-18 00:07:12 +0600

    fix: correct Frameworks import case

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import Card from "../components/Card";
 import { Globe } from "../components/globe";
 import CopyEmailButton from "../components/CopyEmailButton";
-import { Frameworks } from "../components/FrameWorks";
+import { Frameworks } from "../components/Frameworks";
 import useInView from "../hooks/useInView";
 import usePrefersReducedMotion from "../hooks/usePrefersReducedMotion";
 import { useLiteMode } from "../context/LiteModeContext";
